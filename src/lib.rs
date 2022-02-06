5f467c9552d02e513c77fa2885f2c59f981771c0
Ranko <ranko.stevanovich@gmail.com>
2022-02-06 17:18:08 +0300

    basic tga file output

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,40 @@
+use std::fs::File;
+use std::io::Write;
+use std::slice;
+use std::mem;
+use std::io;
+
 #[derive(Copy, Clone, Debug)]
 pub struct TGAColor(pub u8, pub u8, pub u8, pub u8);
+
+#[repr(C, packed)]
+#[derive(Default)]
+struct TGAHeader {
+  id_length: u8,
+  color_map_type: u8,
+  image_type: u8,
+  c_map_start: u16,
+  c_map_length: u16,
+  c_map_depth: u8,
+  x_offset: u16,
+  y_offset: u16,
+  width: u16,
+  height: u16,
+  pixel_depth: u8,
+  image_descriptor: u8,
+}
+
+unsafe fn struct_to_u8_slice<T>(s: &T) -> &[u8] {
+  let data_ptr: *const u8 = mem::transmute(s);
+  let slice = slice::from_raw_parts(data_ptr, mem::size_of::<T>());
+  slice
+}
+
+unsafe fn slice_to_u8_slice<T>(s: &[T]) -> &[u8] {
+  let data_ptr: *const u8 = mem::transmute(&s[0]);
+  let slice = slice::from_raw_parts(data_ptr, mem::size_of::<T>() * s.len());
+  slice
+}
 
 #[derive(Debug)]
 pub struct TGAImage {
@@ -18,20 +53,41 @@ impl TGAImage {
     }
   }
 
-  pub fn set(&mut self, x: usize, y: usize, color: TGAColor) -> bool {
+  pub fn set(&mut self, x: usize, y: usize, color: TGAColor) -> Result<(), &str> {
     if x >= self.width || y >= self.height {
-      return false;
+      return Err("x and y must be within initial image boundaries");
     }
 
     self.data[x + y * self.width] = color;
-    true
+    Ok(())
   }
 
-  pub fn flip_vertically(&mut self) -> bool {
-    true
+  // TODO! Optimize the algorithm. Now O(n) is n^2.
+  pub fn flip_vertically(&mut self) -> () {
+    let half = self.height >> 1;
+    for i in 0..half {
+      for j in 0..self.width {
+        let pixel_idx = i * self.width + j;
+        let pixel_to_swap_x_coord = self.height - 1 - i;
+        let pixel_to_swap_idx = pixel_to_swap_x_coord * self.width + j;
+        self.data.swap(pixel_idx, pixel_to_swap_idx);
+      }
+    }
   }
 
-  pub fn write_tga_file(filename: String) -> bool {
-    true
+  pub fn write_tga_file(&self, filename: &str) -> io::Result<()> {
+    let mut file = File::create(filename).unwrap();
+    let header = TGAHeader {
+      image_type: 2,
+      width: self.width as u16,
+      height: self.height as u16,
+      pixel_depth: 32,
+      ..TGAHeader::default()
+    };
+    unsafe {
+      file.write_all(struct_to_u8_slice(&header)).unwrap();
+      file.write_all(slice_to_u8_slice(&self.data[..])).unwrap();
+    }
+    Ok(())
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use rsgl::{ TGAColor, TGAImage };
 
 fn main() {
-    let white = TGAColor(255, 255, 255, 255);
     let red = TGAColor(255, 0, 0, 255);
 
     let mut image = TGAImage::new(100, 100);
-    image.set(52, 41, red);
-    println!("{:?}", image);
+    image.set(52, 41, red).unwrap();
+    image.flip_vertically();
+    image.write_tga_file("output.tga").unwrap();
 }
