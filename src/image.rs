26f2843c08517f16663a026ec76f62f66b8692c4
Ranko <ranko.stevanovich@gmail.com>
2022-03-22 12:12:42 +0300

    refactor/move render function to image module

--- src/image.rs
+++ src/image.rs
@@ -4,6 +4,8 @@ use std::{ slice, mem };
 use std::io::{ self };
 use std::path::Path;
 
+use crate::model::Model;
+
 #[derive(Copy, Clone, Debug)]
 pub struct TGAColor(pub u8, pub u8, pub u8, pub u8);
 
@@ -151,5 +153,27 @@ impl TGAImage {
       file.write_all(slice_to_u8_slice(&self.data[..])).unwrap();
     }
     Ok(())
+  }
+
+  pub fn render(&mut self, model: &Model, color: TGAColor) -> () {
+    let half_width = self.width as f32/2.;
+    let half_height = self.height as f32/2.;  
+
+    for i in 0..model.n_faces() {
+      let face = model.face(i);
+
+      for j in 0..3 {
+        let v0 = model.vertex(face[j] as usize);
+        let v1 = model.vertex(face[(j + 1) % 3] as usize);
+        let x0 = ((v0.x + 1.) * half_width) as i32;
+        let y0 = ((v0.y + 1.) * half_height) as i32;
+        let x1 = ((v1.x + 1.) * half_width) as i32;
+        let y1 = ((v1.y + 1.) * half_height) as i32;
+
+        self.draw_line(x0, y0, x1, y1, color);
+      }
+    }
+
+    self.write_tga_file(&Path::new("output.tga")).unwrap();
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,28 +9,10 @@ const WHITE: TGAColor = TGAColor(255, 255, 255, 255);
 const WIDTH: i32 = 800;
 const HEIGHT: i32 = 800;
 
-const HALF_WIDTH: f32 = WIDTH as f32/2.;
-const HALF_HEIGHT: f32 = HEIGHT as f32/2.;
-
 fn main() {
     let mut image = TGAImage::new(WIDTH, HEIGHT);
 
-    let model = Model::new(&Path::new("obj/african_head.obj"));    
+    let model = Model::new(&Path::new("obj/african_head.obj"));
 
-    for i in 0..model.n_faces() {
-        let face = model.face(i);
-
-        for j in 0..3 {
-            let v0 = model.vertex(face[j] as usize);
-            let v1 = model.vertex(face[(j + 1) % 3] as usize);
-            let x0 = ((v0.x + 1.) * HALF_WIDTH) as i32;
-            let y0 = ((v0.y + 1.) * HALF_HEIGHT) as i32;
-            let x1 = ((v1.x + 1.) * HALF_WIDTH) as i32;
-            let y1 = ((v1.y + 1.) * HALF_HEIGHT) as i32;
-
-            image.draw_line(x0, y0, x1, y1, WHITE);
-        }
-    }
-
-    image.write_tga_file(&Path::new("output.tga")).unwrap();
+    image.render(&model, WHITE);
 }
